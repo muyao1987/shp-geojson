11b47a8cb638e7222d04600e2b8bb4988506d60e
wanghao <wh@marsgis.cn>
2022-11-17 21:31:37 +0800

    增加编码参数

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-﻿import shp from 'shpjs'
+﻿import shp from '@mars3d/shpjs'
 
 //shp转geojson
-export function toGeoJSON(param) {
-  if (!param) return Promise.reject('参数不能为空')
+export function toGeoJSON(base, whiteList, encoding) {
+  return shp(base, whiteList, encoding)
+}
 
-  return shp(param)
+export function parseShp(shp, prj) {
+  return shp.parseShp(shp, prj)
+}
+
+export function parseDbf(dbf, cpg) {
+  return shp.parseDbf(dbf, cpg)
+}
+
+export function parseZip(buffer, whiteList, encoding) {
+  return shp.parseZip(buffer, whiteList, encoding)
+}
+
+export function getShapefile(base, whiteList, encoding) {
+  return shp.getShapefile(base, whiteList, encoding)
+}
+
+export function combine(arr) {
+  return shp.combine(arr)
 }
